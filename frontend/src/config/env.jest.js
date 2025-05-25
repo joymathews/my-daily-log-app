894b43404832d3795c691544a4af0284bb4e4f5b
Joy Mathews <joymathe@gmail.com>
2025-05-25 19:47:03 +0530

    refactor: simplify env.js by removing unnecessary environment checks and delete unused env.jest.js

--- frontend/src/config/env.jest.js
+++
@@ -1,3 +0,0 @@
-// This file is not needed because Jest will use the manual mock in __mocks__/env.js
-// and the correct env.js is selected by the moduleNameMapper in jest.config.js.
-// Keeping this file may cause confusion.
--- frontend/src/config/env.js
+++ frontend/src/config/env.js
@@ -1,11 +1,2 @@
-// Re-export the correct env file based on environment
-// Jest will use the manual mock, Vite will use env.vite.js
-let viteEnv;
-if (typeof process !== 'undefined' && process.env && process.env.JEST_WORKER_ID) {
-  // In Jest, do nothing (manual mock will be used)
-  viteEnv = {};
-} else {
-  // In Vite, import the real env
-  viteEnv = require('./env.vite.js').default;
-}
-export default viteEnv;
+import viteEnvModule from './env.vite.js';
+export default viteEnvModule;
